8f40caf136d44582ae229d43bbc00270cca84950
赵忍德 <351157970@qq.com>
2016-07-31 17:09:05 +0800

    初始化项目

---
+++ src/Skymate.Log4NetForAbp/Log4NetBootstrapper.cs
@@ -0,0 +1,30 @@
+﻿namespace Skymate
+{
+    using Abp.Dependency;
+
+    using Castle.Facilities.Logging;
+
+    public static class Log4NetBootstrapper
+    {
+        public static void Start(string configName=null)
+        {
+            var config = Log4NetFactory.DefaultConfigFileName;
+
+            if (!string.IsNullOrEmpty(configName))
+            {
+                config = configName;
+            }
+
+            if (IocManager.Instance.IsRegistered<LoggingFacility>())
+            {
+                return;
+            }
+
+            IocManager.Instance.IocContainer
+                .AddFacility<LoggingFacility>(f => f.LogUsing<Log4NetFactory>()
+              .WithConfig(config));
+
+        }
+        
+    }
+}
---
+++ src/Skymate.Log4NetForAbp/Log4NetModule.cs
@@ -0,0 +1,21 @@
+﻿namespace Skymate
+{
+    using System.Reflection;
+
+    using Abp;
+    using Abp.Modules;
+    
+    [DependsOn(typeof(AbpKernelModule))]
+    public class Log4NetModule:AbpModule
+    {
+        public override void PreInitialize()
+        {
+
+        }
+
+        public override void Initialize()
+        {
+            this.IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+        }
+    }
+}
